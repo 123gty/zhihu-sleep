5ca2cba60390d395fe064e5db0f18a34e24f37d9
开江鱼gty <944245073@qq.com>
2022-02-15 15:45:32 +0800

    定时发送，数据可视化提交

--- getZHcontent.py
+++ getZHcontent.py
@@ -47,6 +47,7 @@ def get_pic_url():
 # 统计书籍出现的频率
 def download_picture():
     ifStop=0
+    submit = []
     while True:
         glock.acquire()
         if len(contentList) ==0:
@@ -54,7 +55,9 @@ def download_picture():
             ifStop +=1 
             if ifStop == 2:
                 y2 = {k: v for k, v in sorted(tongjicishu.items(), key=lambda item: item[1], reverse=True)}
-                return y2	
+                for key in y2.keys():
+                    submit.append("<p>"+ key+ ":推荐人数"+str(y2[key])+"人</p>")
+                return ''.join(submit)	
                 break
             else:
                 continue
@@ -66,7 +69,7 @@ def download_picture():
                 tongjicishu[url]=tongjicishu[url]+1
             else :
                 tongjicishu[url]=1
-
+# 
 def main():
     for idx in range(0,math.ceil(totals/5)):
         url = f"https://www.zhihu.com/api/v4/questions/29114634/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cvip_info%2Cbadge%5B%2A%5D.topics%3Bdata%5B%2A%5D.settings.table_of_content.enabled&limit=5&offset={idx*5}&platform=desktop&sort_by=default"
@@ -80,34 +83,38 @@ def main():
         time.sleep(2)
         consumer = threading.Thread(target=download_picture)
         consumer.start()
+def submitPut(putcontent):
+    putUrl = "https://www.zhihu.com/api/v4/answers/2342429808?include=is_visible%2Cpaid_info%2Cpaid_info_content%2Cadmin_closed_comment%2Creward_info%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_normal%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cattachment%2Crelationship.is_authorized%2Cvoting%2Cis_thanked%2Cis_author%2Cis_nothelp%2Cis_recognized%2Cis_labeled%3Bmark_infos%5B*%5D.url%3Bauthor.vip_info%2Cbadge%5B*%5D.topics%3Bsettings.table_of_content.enabled"
+    data = {
+           "content":putcontent,
+           "reshipment_settings":"disallowed",
+           "comment_permission":"all",
+           "reward_setting":{"can_reward":False,"tagline":""},
+           "disclaimer_status":"close",
+           "disclaimer_type":"none",
+           "commercial_report_info":{"is_report":False},
+           "is_report":False,
+           "push_activity":True,
+           "table_of_contents_enabled":False,
+           "thank_inviter_status":"close"
+           }
+    datascontent=json.dumps(data).encode('utf8')
+    # data = urllib.parse.urlencode(formData).encode("utf-8")
+    putres = urllib.request.Request(putUrl,data=datascontent,headers=headers,method='PUT')#请求
+    putres2 = urllib.request.urlopen(putres).read().decode("utf-8")#获取html
 if __name__ == '__main__':
     while True:
         totalsUrl = "https://www.zhihu.com/api/v4/questions/29114634/answers?include=data%5B*%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B*%5D.mark_infos%5B*%5D.url%3Bdata%5B*%5D.author.follower_count%2Cvip_info%2Cbadge%5B*%5D.topics%3Bdata%5B*%5D.settings.table_of_content.enabled&offset=3&limit=5&sort_by=default&platform=desktop"
         res = urllib.request.Request(totalsUrl,headers=headers)#请求
         res2 = urllib.request.urlopen(res).read().decode("utf-8")#获取html
         totals = json.loads(res2)['paging']['totals']#获取数据data
+        submitPut("<p>正在获取。。。</p>")#//提交空内容，防止重复
         print(totals,"__________")
         url_list = []
         contentList = []
         tongjicishu = {}
         main()
-        #修改回答
-        putUrl = "https://www.zhihu.com/api/v4/answers/2342429808?include=is_visible%2Cpaid_info%2Cpaid_info_content%2Cadmin_closed_comment%2Creward_info%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_normal%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cattachment%2Crelationship.is_authorized%2Cvoting%2Cis_thanked%2Cis_author%2Cis_nothelp%2Cis_recognized%2Cis_labeled%3Bmark_infos%5B*%5D.url%3Bauthor.vip_info%2Cbadge%5B*%5D.topics%3Bsettings.table_of_content.enabled"
-        data = {
-               "content":"<p>//python测试</p><p>//主内容发送</p>",
-               "reshipment_settings":"disallowed",
-               "comment_permission":"all",
-               "reward_setting":{"can_reward":false,"tagline":""},
-               "disclaimer_status":"close",
-               "disclaimer_type":"none",
-               "commercial_report_info":{"is_report":false},
-               "is_report":false,
-               "push_activity":true,
-               "table_of_contents_enabled":false,
-               "thank_inviter_status":"close"
-               }
-        datascontent=urllib.parse.urlencode(data).encode('utf-8') 
-        putres = urllib.request.Request(putUrl,data=datascontent,headers=headers,method='PUT')#请求
-        putres2 = urllib.request.urlopen(putres).read().decode("utf-8")#获取html
-        print(download_picture(),putres,99000113213)
+        putcontent = download_picture()
+        submitPut("<p>本话题汇总,目前"+str(totals)+"回答</p>"+"<p>每天"+time.strftime('%H:%M:%S')+"更新</p>"+json.dumps(putcontent,ensure_ascii=False))
+        
         time.sleep(10) #睡眠半个小时
