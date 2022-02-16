6433ccce66b66f2feea88450354f4b2a4656c7c0
开江鱼gty <944245073@qq.com>
2022-02-16 18:10:31 +0800

    //添加注解

--- getZHcontent.py
+++ getZHcontent.py
@@ -74,11 +74,11 @@ def main():
     for idx in range(0,math.ceil(totals/5)):
         url = f"https://www.zhihu.com/api/v4/questions/29114634/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Ccreated_time%2Cupdated_time%2Creview_info%2Crelevant_info%2Cquestion%2Cexcerpt%2Cis_labeled%2Cpaid_info%2Cpaid_info_content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B%2A%5D.mark_infos%5B%2A%5D.url%3Bdata%5B%2A%5D.author.follower_count%2Cvip_info%2Cbadge%5B%2A%5D.topics%3Bdata%5B%2A%5D.settings.table_of_content.enabled&limit=5&offset={idx*5}&platform=desktop&sort_by=default"
         url_list.append(url)
-    #创建两个线程作为生产者，请求
+    #创建十个线程作为生产者，请求
     for x in range(10):
         product = threading.Thread(target=get_pic_url)
         product.start()
-    #创建三个线程作为消费者，统计
+    #创建一个线程作为消费者，统计
     for x in range(1):
         time.sleep(2)
         consumer = threading.Thread(target=download_picture)
